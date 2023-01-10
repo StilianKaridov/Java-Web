aaab20f652a3cc79de8da996864adff9a368d833
StilianKaridov <karidovstilian@gmail.com>
2023-01-10 13:51:27 +0200

    Made method deleteByUsername

--- mobilelele/src/main/java/bg/softuni/springbootintroduction/repository/UserRepository.java
+++ mobilelele/src/main/java/bg/softuni/springbootintroduction/repository/UserRepository.java
@@ -2,11 +2,16 @@ package bg.softuni.springbootintroduction.repository;
 
 import bg.softuni.springbootintroduction.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
+
+    @Transactional
+    void deleteUserByUsername(String username);
 }
